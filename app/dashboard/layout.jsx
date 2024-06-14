bfcf07ffc2384af0039a0eb560fe8b833da0a1a1
Hakuzweyezu Aphrodis <126547349+HAphrodis@users.noreply.github.com>
2024-06-14 14:46:30 +0200

    Navbar addition

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,21 @@
+import Navbar from '@/components/dashboard/navbar/navbar'
+import Sidebar from '@/components/dashboard/sidebar/sidebar'
+import styles from "@/components/dashboard/dashboard.module.css"
+import React from 'react'
+
+const Layout = ({children}) => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.menu}>
+          <Sidebar/>
+        </div>
+        <div className={styles.content}>
+          <Navbar/>
+          {children}
+          
+        </div>
+      </div>
+    )
+  }
+  
+  export default Layout

6f1da9f48b685efb311d008ca3aead1d7e761309
Hakuzweyezu Aphrodis <126547349+HAphrodis@users.noreply.github.com>
2024-06-14 13:10:03 +0200

    remove some files

--- app/page.js
+++ app/page.js
@@ -1,18 +1,12 @@
-// import ECommerce from "@/components/Dashboard/E-commerce";
-import DefaultLayout from "@/components/Layouts/DefaultLaout";
-import React from "react";
 
-export const metadata = {
-  title: "Next.js E-commerce Dashboard Page | NextAdmin - Next.js Dashboard Kit",
-  description: "This is Next.js Home page for NextAdmin Dashboard Kit",
-};
+import React from "react";
 
 export default function Home() {
   return (
     <>
-      <DefaultLayout>
-        {/* <ECommerce /> */}
-      </DefaultLayout>
+     <div>
+      Dashboard
+     </div>
     </>
   );
 }
